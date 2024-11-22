25ae8b5ee60e94fdc6ce93c0236b41fb632570b6
araf821 <araf.ahmed200@gmail.com>
2024-11-22 05:29:20 -0500

    Task 4 Complete: Refactor Education Section Into Its Own Component

---
+++ src/components/EducationSection.tsx
@@ -0,0 +1,22 @@
+function EducationSection() {
+  return (
+    <div className="section">
+      <h2 className="text-xl md:text-2xl font-semibold">Education</h2>
+      <hr className="border-t-2" />
+
+      <div className="space-y-1 w-full">
+        <div className="flex justify-between items-start">
+          <h3 className="md:text-lg font-bold">Computer Science</h3>
+          <p className="text-nowrap">Sep 2012 - Apr 2077</p>
+        </div>
+        <h4 className="max-md:text-sm font-medium">York University</h4>
+        <p className="max-md:text-sm font-light">
+          <span className="font-semibold">Relevant Courses:</span> React
+          Workshop, Advanced OOP, Computer Organization, Discrete Mathematics
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default EducationSection;
