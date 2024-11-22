1267748725bade6269f091bbff5ff9d5e019a81e
araf821 <araf.ahmed200@gmail.com>
2024-11-22 05:34:35 -0500

    Task 6 Complete: Separate Projects Section Component

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,36 +1,13 @@
 import EducationSection from "./EducationSection";
 import Header from "./Header";
+import ProjectsSection from "./ProjectsSection";
 
 function HomePage() {
   return (
     <div className="home-page">
       <Header />
       <EducationSection />
-
-      <div className="section">
-        <h2 className="text-xl md:text-2xl font-semibold">Projects</h2>
-        <hr className="border-t-2" />
-
-        <div className="space-y-1 w-full">
-          <h3 className="md:text-lg font-bold">
-            Instagram - A Social Media Platform
-          </h3>
-          <h4 className="text-xs md:text-sm text-gray-500">
-            React | TypeScript | Java | Python | SQL
-          </h4>
-          <ul className="ml-4 list-disc text-sm">
-            <li>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
-              maiores distinctio ducimus quisquam fugit veritatis.
-            </li>
-            <li>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Laudantium soluta assumenda veniam sapiente commodi ipsam, minus
-              iste rem quasi non?
-            </li>
-          </ul>
-        </div>
-      </div>
+      <ProjectsSection />
     </div>
   );
 }
---
+++ src/components/ProjectsSection.tsx
@@ -0,0 +1,30 @@
+function ProjectsSection() {
+  return (
+    <div className="section">
+      <h2 className="text-xl md:text-2xl font-semibold">Projects</h2>
+      <hr className="border-t-2" />
+
+      <div className="space-y-1 w-full">
+        <h3 className="md:text-lg font-bold">
+          Instagram - A Social Media Platform
+        </h3>
+        <h4 className="text-xs md:text-sm text-gray-500">
+          React | TypeScript | Java | Python | SQL
+        </h4>
+        <ul className="ml-4 list-disc text-sm">
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+            maiores distinctio ducimus quisquam fugit veritatis.
+          </li>
+          <li>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
+            soluta assumenda veniam sapiente commodi ipsam, minus iste rem quasi
+            non?
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectsSection;
